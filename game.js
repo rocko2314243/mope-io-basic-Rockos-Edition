a935f9aa9bf52fe5586653f451ebb81c3e252e84
trollboxeralt <120246187+trollboxeralt@users.noreply.github.com>
2022-12-24 10:59:22 +0200

    Update game.js

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ const gamemode = 0
 
 const maxplayers = 50
 const maxbots = 100
-const timeupgrade = 15 // in seconds
+const timeupgrade = 999 // in seconds
 const maxips = 2
 const botsabilityuse = false
 const collideeveryone = false
