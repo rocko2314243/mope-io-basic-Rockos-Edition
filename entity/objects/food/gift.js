81d033dba166f28deb390e7db8b8321ebc2534c1
rocko2314243 <86879726+rocko2314243@users.noreply.github.com>
2022-12-25 17:32:38 +0200

    Create gift.js

---
+++ entity/objects/food/gift.js
@@ -0,0 +1,49 @@
+
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+
+function gift(id, newid, x, y) {
+    this.class = 'Food'
+    this.isdead = false;
+
+    this.id = id;
+
+    this.radius = randomNumber(20, 30)
+    this.isbiome = false;
+    this.sendhp = false
+    this.type = 85; //object type (animal. hill bush)
+    this.collideable = true
+    this.xp = 99999
+    this.water = 10
+    this.spawned = false
+    this.isloaded = false
+    this.energy = 20
+    this.spawnedby2 = newid
+    this.biome = 1
+    this.movable = true
+    this.speed = 10
+    this.killerid = 0
+    this.isinvisible = false
+    this.x = x
+    this.y = y
+    this.isinquadtree = false
+    this.lowestrespawnsec = 30
+    this.bigestrespawnsec = 60
+    this.veloX = 0
+    this.veloY = 0
+    this.deathtime = 80
+    this.spawnedtime = Date.now();
+};
+
+
+gift.prototype = {
+
+};
+gift.prototype.customdatawriteoncreate = function (buf, off) {
+}
+gift.prototype.customdatawriteonupdate = function (buf, off) {
+}
+
+module.exports = gift;
